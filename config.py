f950639feb757b0576872c193e016168eaa0d2c4
Kyathi29 <kyathirekha@gmail.com>
2026-02-26 11:02:37 +0530

    Fixed secrets handling

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
 
-OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
-RAPID_API_KEY = os.getenv("RAPID_API_KEY")
-ADZUNA_APP_ID = os.getenv("ADZUNA_APP_ID")
-ADZUNA_APP_KEY = os.getenv("ADZUNA_APP_KEY")
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+def get_secret(key):
+    return os.getenv(key) or st.secrets.get(key)
+
+OPENWEATHER_API_KEY = get_secret("OPENWEATHER_API_KEY")
+RAPID_API_KEY = get_secret("RAPID_API_KEY")
+ADZUNA_APP_ID = get_secret("ADZUNA_APP_ID")
+ADZUNA_APP_KEY = get_secret("ADZUNA_APP_KEY")
+GROQ_API_KEY = get_secret("GROQ_API_KEY")
